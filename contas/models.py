7ae843de99d45743404e0f8c3fb0f673689c3137
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2022-07-30 19:39:35 +0200

    create models

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,27 @@
+from tabnanny import verbose
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+# class Person(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     las_name = models.CharField
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+class Transacao(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length=250)
+    valor = models.DecimalField(max_digits=7,decimal_places=2)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True,blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Transacoes'
+    def __str__(self):
+        return self.descricao
--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from .models import Transacao
 
 # Create your views here.
+def home(request):
+    data ={}
+    # data['transacoes'] = ['t1','t2','t3']
+    # data['now'] = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+    # return HttpResponse(html)
+    return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+    data= {}
+    return render(request, 'contas/listagem.html', data)
